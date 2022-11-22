e5b069a884e31ae6e6192a2b2e7b198bfcaa46e6
ShG <everest@Shuhans-MBP.attlocal.net>
2022-11-22 09:45:22 -0800

    Edabit

---
+++ edabit/Edabit-Py_P0001_isPlural.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# --- Edabit-Py_P0001_isPlural
+
+def solution(arg1, arg2):
+    return arg1 + arg2
+
+if __name__ == "__main__":
+    print('----------start------------')
+    arg1 = 12
+    arg2 = -4
+    print(solution( arg1, arg2 ))
+    print('------------end------------')
